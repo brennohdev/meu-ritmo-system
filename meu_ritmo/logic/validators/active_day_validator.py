89334069d222bb3f369ab64cfd755c0d3a7af019
Samuel <colaco.samuel8@gmail.com>
2025-07-11 10:40:53 -0300

    Feat: Adicionando validação

---
+++ meu_ritmo/logic/validators/active_day_validator.py
@@ -0,0 +1,26 @@
+
+from datetime import date
+
+from meu_ritmo.domain.models.task import Tarefa
+
+class DiaAtivo_Validator:
+
+    def validar_data(data: date) -> tuple[bool, str]:
+
+        if not isinstance(data, date):
+            return (False, "Data tem que ser do tipo válido")
+
+        return (True, "")
+    
+    def validar_tarefa(tarefas: list[Tarefa]) -> tuple[bool, str]:
+
+        if not isinstance(tarefas, list):
+            return (False, "Tarefas tem que ser uma lista")
+        
+        for tarefa in tarefas:
+            if not isinstance(tarefa, Tarefa):
+                return (False, "Os itens tem que ser da classe Tarefa")
+        
+        return (True, "")
+
+
---
+++ meu_ritmo/logic/validators/task_validator.py
@@ -0,0 +1,91 @@
+
+from datetime import date
+
+from meu_ritmo.domain.enums.category_enum import CategoriaEnum
+
+class Validacao_Tarefas:
+    def validar_descricao(descricao: str) -> tuple[bool, str]:
+
+        if not isinstance(descricao, str):
+            return (False, "Descrição deve ser do tipo string")
+        
+        if not descricao.strip():
+            return (False, "Descrição deve ter no minimo 1 caracter")
+        
+        return (True, "")
+
+    def validar_prioridade(prioridade: int) -> tuple[bool, str]:
+
+        if not isinstance(prioridade, int):
+            return (False, "Prioridade deve ser um número inteiro")
+        
+        if not prioridade < 0:
+            return (False, "Prioridade não pode ser um número negativo")
+        
+        if not 0 < prioridade <= 3:
+            return (False, "Prioridade acima do limite")
+
+        return (True, "")
+    
+    def validar_impacto_energia(impacto_energia: int) -> tuple[bool, str]:
+
+        if not isinstance(impacto_energia, int):
+            return (False, "Impacto de energia deve ser um número inteiro")
+        
+        return (True, "")
+    
+    def validar_categoria(categoria: CategoriaEnum) -> tuple[bool, str]:
+
+        if not isinstance(categoria, CategoriaEnum):
+            return (False, "Categoria tem que ser dos tipos estabelecidos")
+        return (True, "")
+    
+    def validar_data_de_criacao(data_de_criacao: date) -> tuple[bool, str]:
+
+        if not isinstance(data_de_criacao, date):
+            return (False, "Data tem que ser do tipo válido")
+        
+
+        return (True, "")
+
+    def validar_concluido(concluida: bool) -> tuple[bool, str]:
+
+        if not isinstance(concluida, bool):
+            return (False, "Concluida tem que ser [True/False]")
+        
+        return (True, "")
+    
+    def validar_tarefa_completa(classe, descricao: str, prioridade: int, impacto_energia: int, categoria: CategoriaEnum) -> tuple[bool, list[str]]:
+
+        errors: list[str] = []
+
+        valid_desc, msg_desc = classe.validar_descricao(descricao)
+
+        if not valid_desc:
+            errors.append(msg_desc)
+        
+        valid_prior, msg_prior = classe.validar_prioridade(prioridade)
+
+        if not valid_prior:
+            errors.append(msg_prior)
+        
+        valid_impa, msg_impa = classe.validar_impacto_energia(impacto_energia)
+
+        if not valid_impa:
+            errors.append(msg_impa)
+        
+        valid_categoria, msg_categoria = classe.validar_categoria(categoria)
+
+        if not valid_categoria:
+            errors.append(msg_categoria)
+        
+        return (len(errors) == 0, errors)
+
+
+
+
+
+
+
+
+
